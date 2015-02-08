f65a92d77b03d64178aed01512ab1aa3d152db66
stone725 <stone_725@outlook.jp>
2015-02-08 19:03:00 +0900

    標準入出力などについてを記述

---
+++ C-Sharp-tips/C-Sharp-tips/Hello_World.cs
@@ -0,0 +1,22 @@
+﻿// TODO : 標準出力
+using System;
+
+namespace C_Sharp_tips {
+	class Hello_World {
+		static void Main()
+		{
+			//改行をせずに出力
+			Console.Write("Hello World");
+			//\nで改行ができる
+			Console.Write("\n");
+
+			//また、Console.WriteLineとすれば自動的に改行を行うことが可能である
+			Console.WriteLine("Hello World");
+			Console.WriteLine(50); //50
+			//また""で囲った範囲の中に{0},{1}のように{(0から始まって連続した数値)}として終わりの"のあとに,(内容)とすればその内容を表示することができる
+			Console.WriteLine("{0}", "Hello World");
+			//数値なども読み込んで表示してくれる
+			Console.WriteLine("{0}", 100); //100
+		}
+	}
+}
---
+++ C-Sharp-tips/C-Sharp-tips/Input.cs
@@ -0,0 +1,29 @@
+﻿// TODO : 変数の扱い、入力の受け取り
+using System;
+
+namespace C_Sharp_tips
+{
+	public class Input
+	{
+		static void Main()
+		{
+			//int (変数名)とすれば32bit符号付き整数の変数を宣言できる
+			int value;
+			//(変数名) = (値)とすることにより数値を代入できる
+			value = 40;
+			//Write,WriteLineは自動的に変数の値を自動的に読み取って表示してくれる
+			Console.WriteLine("value = {0}", value); //40
+			//string (変数名)とすれば文字列の変数を代入することができる
+			string str = "Hello World";
+			Console.WriteLine("str = {0}", str); //Hello World
+			//(変数名) = Console.ReadLineとすることにより変数に入力した文字列を一行分受け取ることができる
+			str = Console.ReadLine();
+			Console.WriteLine("str = {0}", str); //入力した文字列が表示される
+			//数値に入力の値を入れたい場合はint.Parseとすると数値化した値を受け取ることができる
+			value = int.Parse(Console.ReadLine());
+			Console.WriteLine("value = {0}", value);
+			//表示する際に演算も行うことができる
+			Console.WriteLine("value + 4 = {0}", value + 4);
+		}
+	}
+}
